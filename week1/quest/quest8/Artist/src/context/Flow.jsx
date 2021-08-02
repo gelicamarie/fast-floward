9452803e1bed19cc4f10db9237a9eba8beb73b3a
gelicamarie <turla_angelica@yahoo.com>
2021-08-02 15:56:23 -0400

    updated Flow.jsx

--- week1/quest/quest8/Artist/src/context/Flow.jsx
+++ week1/quest/quest8/Artist/src/context/Flow.jsx
@@ -1,3 +1,7 @@
+/**
+ * Artist Client App uses Flow.jsk as a single point of
+ * contact with the flow blockchain
+ */
 import React, {useReducer, useEffect, useCallback} from 'react';
 import * as fcl from '@onflow/fcl';
 import * as FlowTypes from '@onflow/types';
@@ -5,12 +9,6 @@ import * as FlowTypes from '@onflow/types';
 import Picture from '../model/Picture.js';
 
 const Context = React.createContext({});
-
-// const constants = {
-//   flowFormat: new Intl.NumberFormat('en-US', {
-//     minimumFractionDigits: 4
-//   })
-// };
 
 function reducer(state, action) {
   switch (action.type) {
@@ -89,51 +87,62 @@ function Provider(props) {
   );
   const createCollection = useCallback(
     async () => {
-      const transactionId = await fcl.send([
-        fcl.transaction`
-          import LocalArtist from 0x6d5dc972b1464232
-          
-          transaction() {
-            prepare(account: AuthAccount) {
-              account.save<@LocalArtist.Collection>(
-                <- LocalArtist.createCollection(),
-                to: /storage/LocalArtistPictureCollection
-              )
-              account.link<&{LocalArtist.PictureReceiver}>(
-                /public/LocalArtistPictureReceiver,
-                target: /storage/LocalArtistPictureCollection
-              )
+        const transactionId = await fcl
+          .send([
+            fcl.transaction`
+            import LocalArtist from 0x968eaba38619f209
+
+            transaction() {
+              prepare(account: AuthAccount) {
+                account.save<@LocalArtist.Collection>(
+                  <- LocalArtist.createCollection(),
+                  to: /storage/LocalArtistPictureCollection
+                )
+                account.link<&{LocalArtist.PictureReceiver}>(
+                  /public/LocalArtistPictureReceiver,
+                  target: /storage/LocalArtistPictureCollection
+                )
+              }
             }
-          }
-        `,
-        fcl.payer(fcl.authz),
-        fcl.proposer(fcl.authz),
-        fcl.authorizations([fcl.authz]),
-        fcl.limit(9999)
-      ]).then(fcl.decode);
-      
-      return fcl.tx(transactionId).onceSealed();
+            `,
+            fcl.payer(fcl.authz),
+            fcl.proposer(fcl.authz),
+            fcl.authorizations([fcl.authz]),
+            fcl.limit(9999),
+          ])
+          .then(fcl.decode);
+
+        return fcl.tx(transactionId).onceSealed();
+        
     },
     []
   );
   const destroyCollection = useCallback(
     async () => {
-      const transactionId = await fcl.send([
-        fcl.transaction`
-          import LocalArtist from 0x6d5dc972b1464232
+      // TODO: Implement the destroyCollection.cdc transaction using "fcl.send".
+
+      
+      const transactionId = await fcl
+        .send([
+          fcl.transaction`
+          import LocalArtist from 0x968eaba38619f209
+
           transaction() {
             prepare(account: AuthAccount) {
               account.unlink(/public/LocalArtistPictureReceiver)
-              let collection <- account.load<@LocalArtist.Collection>(from: /storage/LocalArtistPictureCollection)
+              let collection <- account.load<@LocalArtist.Collection>(
+                from: /storage/LocalArtistPictureCollection
+              )
               destroy collection
             }
           }
-        `,
-        fcl.payer(fcl.authz),
-        fcl.proposer(fcl.authz),
-        fcl.authorizations([fcl.authz]),
-        fcl.limit(9999)
-      ]).then(fcl.decode);
+          `,
+          fcl.payer(fcl.authz),
+          fcl.proposer(fcl.authz),
+          fcl.authorizations([fcl.authz]),
+          fcl.limit(9999),
+        ])
+        .then(fcl.decode);
 
       return fcl.tx(transactionId).onceSealed();
     },
@@ -156,22 +165,28 @@ function Provider(props) {
             ]);
           }
           
-          const collection = await fcl.send([
-            fcl.script`
-              import LocalArtist from 0x6d5dc972b1464232
-      
+          // TODO: Implement the getCollections.cdc script using "fcl.script", and
+          // the "args" in place for the script's arguments.
+          // Use the "fetchBalance" as an example.
+
+          const collection = await fcl
+            .send([
+              fcl.script`
+              import LocalArtist from 0x968eaba38619f209
+
               pub fun main(address: Address): [LocalArtist.Canvas] {
                 let account = getAccount(address)
                 let pictureReceiverRef = account
                   .getCapability<&{LocalArtist.PictureReceiver}>(/public/LocalArtistPictureReceiver)
                   .borrow()
-                  ?? panic("Couldn't borrow picture receiver reference.")
+                  ?? panic("Couldn't borrow Picture Receiver reference.")
               
                 return pictureReceiverRef.getCanvases()
               }
-            `,
-            args
-          ]).then(fcl.decode);
+              `
+            ])
+            .then(fcl.decode);
+
           const mappedCollection = collection.map(
             (serialized) => new Picture(
               serialized.pixels,
@@ -198,16 +213,19 @@ function Provider(props) {
   );
   const printPicture = useCallback(
     async (picture) => {
-      const transactionId = await fcl.send([
-        fcl.transaction`
-          import LocalArtist from 0x6d5dc972b1464232
+      const transactionId = await fcl
+        .send([
+          fcl.transaction`
+      
+          import LocalArtist from 0x968eaba38619f209
+          
           transaction(width: Int, height: Int, pixels: String) {
-            
+  
             let picture: @LocalArtist.Picture?
             let collectionRef: &{LocalArtist.PictureReceiver}
+          
             prepare(account: AuthAccount) {
-              // TODO: Change to your contract account address.
-              let printerRef = getAccount(0x6d5dc972b1464232)
+              let printerRef = getAccount(0x968eaba38619f209)
                 .getCapability<&LocalArtist.Printer>(/public/LocalArtistPicturePrinter)
                 .borrow()
                 ?? panic("Couldn't borrow printer reference.")
@@ -230,152 +248,39 @@ function Provider(props) {
               }
             }
           }
-        `,
-        fcl.args([
-          fcl.arg(picture.width, FlowTypes.Int),
-          fcl.arg(picture.height, FlowTypes.Int),
-          fcl.arg(picture.pixels, FlowTypes.String)
-        ]),
-        fcl.payer(fcl.authz),
-        fcl.proposer(fcl.authz),
-        fcl.authorizations([fcl.authz]),
-        fcl.limit(9999)
-      ]).then(fcl.decode);
-      
+          
+          `,
+          fcl.args([
+            fcl.arg(picture.width, FlowTypes.Int),
+            fcl.arg(picture.height, FlowTypes.Int),
+            fcl.arg(picture.pixels, FlowTypes.String),
+          ]),
+          fcl.payer(fcl.authz),
+          fcl.proposer(fcl.authz),
+          fcl.authorizations([fcl.authz]),
+          fcl.limit(9999),
+        ])
+        .then(fcl.decode);
       return fcl.tx(transactionId).onceSealed();
+      
     },
     []
   );
-  // const fetchListings = useCallback(
-  //   async () => {
-  //     const listings = await fcl.send([
-  //       fcl.script`
-  //         import LocalArtistMarket from 0x6d5dc972b1464232
-  
-  //         pub fun main(): [LocalArtistMarket.Listing] {
-  //           let account = getAccount(0x6d5dc972b1464232)
-  //           let marketInterfaceRef = account
-  //             .getCapability(/public/LocalArtistMarket)
-  //             .borrow<&{LocalArtistMarket.MarketInterface}>()
-  //             ?? panic("Couldn't borrow market interface reference.")
-          
-  //           return marketInterfaceRef.getListings()
-  //         }
-  //       `
-  //     ]).then(fcl.decode);
-
-  //     return listings;
-  //   },
-  //   []
-  // );
-  // const postListing = useCallback(
-  //   async (picture, price) => {
-  //     const transactionId = await fcl.send([
-  //       fcl.transaction`
-  //         import LocalArtist from 0x6d5dc972b1464232
-  //         import LocalArtistMarket from 0x6d5dc972b1464232
-  //         transaction(pixels: String, price: UFix64) {
-            
-  //           let picture: @LocalArtist.Picture?
-  //           let seller: Address
-  //           let marketRef: &{LocalArtistMarket.MarketInterface}
-  //           prepare(account: AuthAccount) {
-  //             // TODO: Change to your contract account address.
-  //             self.marketRef = getAccount(0x6d5dc972b1464232)
-  //               .getCapability(/public/LocalArtistMarket)
-  //               .borrow<&{LocalArtistMarket.MarketInterface}>()
-  //               ?? panic("Couldn't borrow market reference.")
-              
-  //             let collection <- account.load<@LocalArtist.Collection>(from: /storage/LocalArtistPictureCollection)!
-  //             self.picture <- collection.withdraw(pixels: pixels)
-  //             account.save<@LocalArtist.Collection>(<- collection, to: /storage/LocalArtistPictureCollection)
-  //             self.seller = account.address
-  //           }
-  //           execute {
-  //             if self.picture == nil {
-  //               destroy self.picture
-  //             } else {
-  //               self.marketRef.sell(picture: <- self.picture!, seller: self.seller, price: price)
-  //             }
-  //           }
-  //         }
-  //       `,
-  //       fcl.args([
-  //         fcl.arg(picture.pixels, FlowTypes.String),
-  //         fcl.arg(`${constants.flowFormat.format(price)}`, FlowTypes.UFix64),
-  //       ]),
-  //       fcl.payer(fcl.authz),
-  //       fcl.proposer(fcl.authz),
-  //       fcl.authorizations([fcl.authz]),
-  //       fcl.limit(9999)
-  //     ]).then(fcl.decode);
-      
-  //     return fcl.tx(transactionId).onceSealed();
-  //   },
-  //   []
-  // );
-  // const withdrawListing = useCallback(
-  //   async (listingIndex) => {
-  //     const transactionId = await fcl.send([
-  //       fcl.transaction`
-  //         import LocalArtist from 0x6d5dc972b1464232
-  //         import LocalArtistMarket from 0x6d5dc972b1464232
-  //         // TODO: Complete this transaction by calling LocalArtistMarket.withdraw().
-  //         transaction(listingIndex: Int) {
-  //         }
-  //       `,
-  //       fcl.args([
-  //         fcl.arg(listingIndex, FlowTypes.Int)
-  //       ]),
-  //       fcl.payer(fcl.authz),
-  //       fcl.proposer(fcl.authz),
-  //       fcl.authorizations([fcl.authz]),
-  //       fcl.limit(9999)
-  //     ]).then(fcl.decode);
-      
-  //     return fcl.tx(transactionId).onceSealed();
-  //   },
-  //   []
-  // );
-  // const buy = useCallback(
-  //   async (listingIndex) => {
-  //     const transactionId = await fcl.send([
-  //       fcl.transaction`
-  //         import LocalArtist from 0x6d5dc972b1464232
-  //         import LocalArtistMarket from 0x6d5dc972b1464232
-  //         import FungibleToken from 0x9a0766d93b6608b7
-  //         import FlowToken from 0x7e60df042a9c0868
-  //         // TODO: Complete this transaction by calling LocalArtistMarket.buy().
-  //         transaction(listingIndex: Int) {
-  //         }
-  //       `,
-  //       fcl.args([
-  //         fcl.arg(listingIndex, FlowTypes.Int)
-  //       ]),
-  //       fcl.payer(fcl.authz),
-  //       fcl.proposer(fcl.authz),
-  //       fcl.authorizations([fcl.authz]),
-  //       fcl.limit(9999)
-  //     ]).then(fcl.decode);
-      
-  //     return fcl.tx(transactionId).onceSealed();
-  //   },
-  //   []
-  // );
 
   const setUser = (user) => {
     dispatch({type: 'setUser', payload: user});
   };
   const logIn = () => {
-    fcl.logIn();
+    fcl.logIn()
+  
   };
   const logOut = () => {
-    fcl.unauthenticate();
+    fcl.unauthenticate()
   };
 
   useEffect(() => {
-    fcl.currentUser().subscribe(setUser);
-  }, []);
+    fcl.currentUser().subscribe(setUser)}
+    ,[]);
 
   useEffect(() => {
     if (state.user && state.user.addr) {
@@ -396,11 +301,7 @@ function Provider(props) {
         fetchCollection,
         createCollection,
         destroyCollection,
-        printPicture,
-        // fetchListings,
-        // postListing,
-        // withdrawListing,
-        // buy
+        printPicture
       }}
     >
       {props.children}
